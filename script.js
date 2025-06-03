ab4eb4e79998daa84bf7e947ab6f080dd66402ad
raihann-git <reyhanfithra@gmail.com>
2025-06-03 23:42:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav')
+
+// ketika hamburger menu di klik
+
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
